52adab2151bbdb193037fd0424226517e45c9425
Oleksandr Anufriyev <oanufriyev@gmail.com>
2018-01-17 00:22:57 +0100

    Create Flights.r

---
+++ Flights.r
@@ -0,0 +1,25 @@
+Country_WebSite <-c("de","com")
+Airports<-c("muc","lis")
+Starting <-"180509"
+Length_of_stay <-seq()
+seq.Date(180509,180515)
+
+          https://www.skyscanner.
+          de
+          /transport/fluge/
+          muc
+          /
+          lis
+          /
+          180509
+          /
+          180516
+          ?adults=
+          2
+          &children=
+          1
+          &adultsv2=
+          2
+          &childrenv2=3&infants=0&cabinclass=economy&rtn=1&
+          preferdirects=false
+          &outboundaltsenabled=false&inboundaltsenabled=false&ref=home#results
