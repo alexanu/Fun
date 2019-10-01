448c4411a692de05c345d55fec9f4c3b31bdeb12
Oleksandr Anufriyev <oanufriyev@gmail.com>
2019-10-01 09:40:25 +0200

    Update diff_schedulers.py

--- diff_schedulers.py
+++ diff_schedulers.py
@@ -22,3 +22,29 @@ def main():
 
 if __name__ == '__main__':
 	main()
+	
+	
+	
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# Scheduler imports
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+# Job imports
+import main as job
+from emails import email_login
+
+def bob_job():
+
+    me, password = email_login()
+    sched = BlockingScheduler()
+
+    @sched.scheduled_job('cron', day_of_week='mon,tue,wed,thu,fri', hour=17)
+    def scheduled_job():
+        job.run(me, password)
+
+    sched.start()    
+
+
+if __name__ == '__main__':
+    bob_job()
