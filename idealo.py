02f98ca12355d772afd39d7e39197c55cfd66ebf
kalmar <oanufriyev@gmail.com>
2019-10-01 21:25:38 +0200

    x

--- idealo.py
+++ idealo.py
@@ -40,11 +40,9 @@ destin = soup.find_all('div',class_="deals-card-destination-container")
 for el1, el2, el3 in [destin, flight_date, price]:
      print(el1['title'], el2.text.split(" - "), el3.text)
 
-describe(price)
-len(price), len(flight_date),len(destin)
 	
 while i<len(price):
-    	
+price[i].text    	
 
 
 etfName = str(etfName.text)
@@ -54,8 +52,31 @@ for elem in flight_date:
      print(elem.text.split(" - "))
 
 
-flight_date = soup.find_all('div',{"class" : "deals-card-date", "class" : "deals-card-destination-container"})
-
 titlesarray = []
 for title in flight_date:
     titlesarray.append(title.text)
+
+
+out=[]
+for i in range(len(rows)):
+	td=rows[i].find_all('td')
+	out=out+[x.text for x in td]
+
+
+
+# Lists to store the scraped data in
+price = []
+flight_date = []
+destin = []
+
+trips = soup.find_all('div', class_ = 'deals-card deals-card--default')
+for trip in trips:
+    price = soup.find('span',class_="deals-card-offer-price").text.str()
+    price.append(price)
+
+    flight_date = soup.find('div',class_="deals-card-date").text.split(" - ")
+    flight_date.append(flight_date)
+
+    destin = soup.find('div',class_="deals-card-destination-container")['title']
+    destin.append(destin)
+
