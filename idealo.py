ac92cdee28b201f9e50c861f32f2fa33331ea9a4
kalmar <oanufriyev@gmail.com>
2019-10-05 01:40:37 +0200

    x

--- idealo.py
+++ idealo.py
@@ -1,35 +1,29 @@
 
-Parsing tickets:
-
-exampleURL = "https://flug.idealo.de/deals/fernreisen/"
-sourceStruct = "view-source:https://flug.idealo.de/deals/fernreisen/"
-
-import html5lib
-import requests
-import urllib2
-import csv
-
-import ast
-import re
-
-
-
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
-
-import time
 import pandas as pd
-import numpy as np
+import datetime
 
 
 mainURL = "https://flug.idealo.de/deals/"
-keyWords = {"herbstferien", "fernreisen", "sommerferien", "best-in-europe", "last-minute", "staedtereisen", "kurzurlaub", "warme-reiseziele"}
+keyWords = {"herbstferien", "fernreisen", "sommerferien", "best-in-europe", "last-minute", 
+			"staedtereisen", "kurzurlaub", "warme-reiseziele"}
+
+not_interested = ['Alghero', 'Belgrad', 'Bergen', 'Berlin Tegel', 'Bukarest', 
+				'Cluj', 'Delhi', 'Detroit', 'Düsseldorf', 'Hamburg', 'Jakarta', 
+				'Köln/Bonn', 'London Gatwick', 'London Heathrow', 'London Luton', 
+				'London Stansted', 'Lviv', 'Manchester', 'Monastir', 'München', 
+				'Pittsburgh', 'Podgorica', 'Pristina', 'Rostock', 'Tirana', 
+				'Ankara', 'Antalya', 'Istanbul', 'Istanbul Sabiha Gökcen', 'Izmir', 
+				'Simferopol', 'Sofia', 'Taschkent', 'Ulan Bator', 'Varna', 'Warschau']
+
+
 
 prices = []
 flight_dates = []
 destins = []
 groups =[]
-
+status=[]
 
 for keyword in keyWords:
 		url = mainURL+keyword
@@ -44,18 +38,42 @@ for keyword in keyWords:
 			destin = trip.find('div',class_="deals-card-destination-container")['title']
 			destins.append(destin)
 			groups.append(keyword)
+			
 
 angebots = pd.DataFrame({'Destination': destins,
-						'Datea': flight_dates,
+						'Dates': flight_dates,
 						'Price': prices,
 						'Group': groups
 						})
-angebots=angebots[angebots['Destination'].str.contains('Münc')|
-					angebots['Destination'].str.contains('Memm')]
-angebots["Destination"]=angebots["Destination"].str.split(' - ').str[1]
-angebots["Period"]=pd.to_datetime(ang["Datea"].str[0], format='%d.%m.%Y').dt.strftime('%Y-%m')
-angebots.sort_values(by=['Group', 'Period'], inplace=True)
-angebots.to_csv("Travel.csv",sep=";")
+angebots=angebots[angebots['Destination'].str.contains('Münc')| # keep only the flights from Munich ...
+					angebots['Destination'].str.contains('Memm')] # ... and Memmingen
+angebots["Destination"]=angebots["Destination"].str.split(' - ').str[1] # after splitting keep only 2nd part: destination
+angebots = angebots[~angebots['Destination'].isin(not_interested)] # removing not interesting destinations
+angebots["Period"]=pd.to_datetime(angebots["Dates"].str[0], format='%d.%m.%Y').dt.strftime('%Y-%m') # new column: month of flight
+angebots['Status'] = str(datetime.date.today()) # new column: when the query was done
+angebots.to_csv("Travel.csv", # creating new file
+				sep=";", 
+				index=False) # we need to eliminate the index column
 
-import os
-os.getcwd()
+
+
+# angebots.sort_values(by=['Group', 'Period'], inplace=True)
+
+
+'''
+angebots.to_csv("Travel.csv", # the file exists already
+				mode='a', # append mode: puts new df in the end of csv
+				sep=";", 
+				header=False, # as we are appending, the file has headers already
+				index=False) # we need to eliminate the index column
+
+
+ang = angebots.copy()
+ang["Destination"]=ang["Destination"].str.split(' - ').str[1]
+ang['Status'] = str(datetime.date.today())
+ang["Period"]=pd.to_datetime(ang["Datea"].str[0], format='%d.%m.%Y').dt.strftime('%Y-%m')
+ang = ang[~ang['Destination'].isin(not_interested)]
+'''
+
+# cities_db_url="https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.5.zip"
+# cities_db = pd.read_csv(cities_db_url)
