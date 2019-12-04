dc7dc0c304308c7419e505d7f117a4df671722ca
kalmar <oanufriyev@gmail.com>
2019-12-04 02:04:26 +0100

    f

--- idealo.py
+++ idealo.py
@@ -4,20 +4,25 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import datetime
 
+import os
+os.getcwd()
+
+directory = 'c:\\Users\\oanuf\\GitHub\\Fun\\'
 
 
-all=pd.read_csv("travel.csv",sep=";") # reading current status
+all=pd.read_csv(directory+"Travel.csv",sep=";") # reading current status
 
 # Filling in empty regios from previous run
-Regio=pd.read_csv("regio.csv",sep=";", index_col='Destination') # reading mapping for regions
+Regio=pd.read_csv(directory+"regio.csv",sep=";", index_col='Destination') # reading mapping for regions
 if len(all[all.isna().any(axis=1)])>0: # if there is not-filled region from previous run ...
     	all.loc[all.isna().any(axis=1),'Regio']  = all.Destination.map(Regio.Regio) # ... vlookup it from updated region mapping
 
 mainURL = "https://flug.idealo.de/deals/"
 keyWords = {"herbstferien", "fernreisen", "sommerferien", "best-in-europe", "last-minute", 
-			"staedtereisen", "kurzurlaub", "warme-reiseziele"}
-
-# not_interested = pd.read_csv("not_interesting.csv")
+			"staedtereisen", "kurzurlaub", "warme-reiseziele", "weihnachtsferien", "weihnachten"
+			"beste-urlaubsziele-des-jahres", "osterferien"}
+not_interested.values
+not_interested = pd.read_csv(directory+"not_interesting.csv")
 not_interested = ['Alghero', 'Belgrad', 'Bergen', 'Berlin Tegel', 'Bukarest', 'Banja Luka',
 		   'Cluj', 'Frankfurt', 'Kishinev', 'Skopje', 'Moskau Scheremetjewo', 'Bremen', 
 		   'Prag', 'Delhi', 'Detroit', 'Hatay', 'Düsseldorf', 'Hamburg', 'Jakarta', 
@@ -35,18 +40,21 @@ groups =[]
 status=[]
 
 for keyword in keyWords:
-		url = mainURL+keyword
-		page = urlopen(url)
-		soup = bs(page)
-		trips = soup.find_all('div', class_ = 'deals-card deals-card--default')
-		for trip in trips:
-			price = trip.find('span',class_="deals-card-offer-price").text
-			prices.append(price)
-			flight_date = trip.find('div',class_="deals-card-date").text.split(" - ")
-			flight_dates.append(flight_date)
-			destin = trip.find('div',class_="deals-card-destination-container")['title']
-			destins.append(destin)
-			groups.append(keyword)
+		try:
+			url = mainURL+keyword
+			page = urlopen(url)
+			soup = bs(page)
+			trips = soup.find_all('div', class_ = 'deals-card deals-card--default')
+			for trip in trips:
+				price = trip.find('span',class_="deals-card-offer-price").text
+				prices.append(price)
+				flight_date = trip.find('div',class_="deals-card-date").text.split(" - ")
+				flight_dates.append(flight_date)
+				destin = trip.find('div',class_="deals-card-destination-container")['title']
+				destins.append(destin)
+				groups.append(keyword)
+		except:
+			pass
 			
 
 angebots = pd.DataFrame({'Destination': destins,
@@ -57,7 +65,7 @@ angebots = pd.DataFrame({'Destination': destins,
 angebots=angebots[angebots['Destination'].str.contains('Münc')| # keep only the flights from Munich ...
 					angebots['Destination'].str.contains('Memm')] # ... and Memmingen
 angebots["Destination"]=angebots["Destination"].str.split(' - ').str[1] # after splitting keep only 2nd part: destination
-angebots = angebots[~angebots['Destination'].isin(not_interested)] # removing not interesting destinations
+angebots = angebots[~angebots['Destination'].isin(not_interested.Destination.values)] # removing not interesting destinations
 angebots["Period"]=pd.to_datetime(angebots["Dates"].str[0], format='%d.%m.%Y').dt.strftime('%Y-%m') # new column: month of flight
 angebots['Status'] = str(datetime.date.today()) # new column: when the query was done
 angebots["Destination"]=angebots["Destination"].replace({'-': ' ', ',': ''}, regex=True) # some destinations contain bad-for-csv symbols
@@ -66,7 +74,7 @@ angebots['Regio'] = angebots.Destination.map(Regio.Regio) # vlookuping region
 
 new=angebots[angebots.isnull().Regio]['Destination'].drop_duplicates() # new destinations
 if len(new)>0:
-	new.to_csv("regio.csv", # the file exists already
+	new.to_csv(directory+"regio.csv", # the file exists already
 				mode='a', # append mode: puts new df in the end of csv
 				sep=";", 
 				header=False, # as we are appending, the file has headers already
@@ -95,11 +103,11 @@ all.to_csv("Travel.csv", # creating new file
 # angebots.sort_values(by=['Group', 'Period'], inplace=True)
 
 
-#all.to_csv("Travel.csv", # the file exists already
-#			mode='a', # append mode: puts new df in the end of csv
-#			sep=";", 
-#			header=False, # as we are appending, the file has headers already
-#			index=False) # we need to eliminate the index column
+all.to_csv(directory+"Travel.csv", # the file exists already
+			mode='a', # append mode: puts new df in the end of csv
+			sep=";", 
+			header=False, # as we are appending, the file has headers already
+			index=False) # we need to eliminate the index column
 
 # cities_db_url="https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.5.zip"
 # cities_db = pd.read_csv(cities_db_url)
@@ -109,6 +117,7 @@ all.to_csv("Travel.csv", # creating new file
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import os
 
 email = EMAIL1
 password = PASS1
