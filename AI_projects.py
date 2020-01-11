f9e8c27c50368b095082bd64026e671ba8508779
kalmar <oanufriyev@gmail.com>
2020-01-11 22:46:03 +0100

    d

--- AI_projects.py
+++ AI_projects.py
@@ -5,34 +5,31 @@ import datetime
 
 mainURL = "https://deepindex.org/"
 
-
 topic = []
 size=[]
+projects=[]
+groups =[]
 
 page = urlopen(mainURL)
 soup = bs(page)
 topics = soup.find_all('h3')
 for topic in topics:
-    print(topic.text)
-	
+	project = topic.find('p').text
+	projects.append(project)
+	groups.append(topic)
 
-topic = []
-size=[]
-flight_dates = []
-destins = []
-groups =[]
-status=[]
+deep = pd.DataFrame({'Category': groups,
+					 'Projects': projects
+						})
 
-for keyword in keyWords:
-		url = mainURL+keyword
-		page = urlopen(url)
-		soup = bs(page)
-		trips = soup.find_all('div', class_ = 'deals-card deals-card--default')
-		for trip in trips:
-			price = trip.find('span',class_="deals-card-offer-price").text
-			prices.append(price)
-			flight_date = trip.find('div',class_="deals-card-date").text.split(" - ")
-			flight_dates.append(flight_date)
-			destin = trip.find('div',class_="deals-card-destination-container")['title']
-			destins.append(destin)
-			groups.append(keyword)
+deep['Status'] = str(datetime.date.today()) # new column: when the query was done
+
+
+#--------------------------------------------------------------------------------------
+
+topics = []
+topics = soup.find_all("div", class_="col-md-4")
+for topic in topics:
+    print(topic.find('p').text)
+
+
