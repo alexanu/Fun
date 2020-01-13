88df70fd1ca43130c43cacff0d23d682de10efdb
kalmar <oanufriyev@gmail.com>
2020-01-13 01:48:09 +0100

    f

--- parsing_snippets.py
+++ parsing_snippets.py
@@ -1,4 +1,7 @@
 
+#Checking the chapters of Zorro
+
+
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as bs # BS parses HTML into an easy machine readable tree format to extract DOM Elements quickly. 
 import html5lib
@@ -15,6 +18,26 @@ import csv
 
 import ast
 import re
+
+
+#-------------------------------------------------------------------------------
+#Checking the chapters of Zorro
+
+directory = 'c:\\Users\\oanuf\\GitHub\\Fun\\'
+zorro_content = "http://www.zorro-trader.com/manual/ht_contents.htm"
+page = urlopen(zorro_content)
+soup = bs(page)
+zorro_pages_links = [a.get('href') for a in soup.find_all('a', href=True)]
+zorro_pages_names = [a.get('title') for a in soup.find_all('a')]
+df = pd.DataFrame({'Names':zorro_pages_names,
+				    'Link':zorro_pages_links})
+df["Link"] = 'https://www.zorro-trader.com/manual/' + df["Link"].astype(str)
+df.to_csv(directory+"Zorro_content.csv", # creating new file
+			sep=";", 
+			index=False) # we need to eliminate the index column
+
+
+
 
 # ----------------------- 1st code block ---------------------------------------
 
