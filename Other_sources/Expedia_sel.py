f5c16a5df1f5ed3bdb5d7ce757d5e145dd033e62
Oleksandr Anufriyev <oanufriyev@gmail.com>
2020-03-02 17:57:54 +0100

    Update Expedia_sel.py

--- Other_sources/Expedia_sel.py
+++ Other_sources/Expedia_sel.py
@@ -510,3 +510,220 @@ if __name__=="__main__":
 	get_flights(driver, url, is_round_trip)
 
 	driver.close()
+	
+# --------------------------------------------------------------------------------------------------------------------------
+# Source: https://github.com/Shruti-Pattajoshi/Travel-Website-Scraping-for-the-Cheapest-Fares/blob/master/flights_scraping.py
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import pandas as pd
+
+import time
+import datetime
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+#webdriver location in my system /path/
+browser = webdriver.Chrome(executable_path='C:/Users/WORK/Desktop/SECOND YEAR PROJECTS/chromedriver.exe')
+
+#choosing the type of ticket
+return_ticket = "//label[@id='flight-type-roundtrip-label-hp-flight']"
+one_way_ticket = "//label[@id='flight-type-one-way-label-hp-flight']"
+multi_ticket = "//label[@id='flight-type-multi-dest-label-hp-flight']"
+
+
+#choosing the type of ticket
+def ticket_chooser(ticket):
+    try:
+        ticket_type = browser.find_element_by_xpath(ticket)
+        ticket_type.click()
+    except Exception as e:
+        pass
+
+#choosing the source place
+def dep_country_chooser(dep_country):
+    fly_from = browser.find_element_by_xpath("//input[@id='flight-origin-hp-flight']")
+    time.sleep(1)
+    fly_from.clear()
+    time.sleep(1.5)
+    fly_from.send_keys('  ' + dep_country)
+    time.sleep(1.5)
+    first_item = browser.find_element_by_xpath("//a[@id='aria-option-0']")
+    time.sleep(1.5)
+    first_item.click()
+
+#choosing the destination place
+def arrival_country_chooser(arrival_country):
+    fly_to = browser.find_element_by_xpath("//input[@id='flight-destination-hp-flight']")
+    time.sleep(1)
+    fly_to.clear()
+    time.sleep(1.5)
+    fly_to.send_keys('  ' + arrival_country)
+    time.sleep(1.5)
+    first_item = browser.find_element_by_xpath("//a[@id='aria-option-0']")
+    time.sleep(1.5)
+    first_item.click()
+
+#choosing the departure date
+def dep_date_chooser(month, day, year):
+    dep_date_button = browser.find_element_by_xpath("//input[@id='flight-departing-hp-flight']")
+    dep_date_button.clear()
+    dep_date_button.send_keys(month + '/' + day + '/' + year)
+
+#choosing the return date
+def return_date_chooser(month, day, year):
+    return_date_button = browser.find_element_by_xpath("//input[@id='flight-returning-hp-flight']")
+    for i in range(11):
+        return_date_button.send_keys(Keys.BACKSPACE)
+    return_date_button.send_keys(month + '/' + day + '/' + year)
+
+
+def search():
+    search = browser.find_element_by_xpath("//button[@class='btn-primary btn-action gcw-submit']")
+    search.click()
+    time.sleep(15)
+    print('Results ready!')
+
+
+df = pd.DataFrame()
+
+#compiling the data to form the pandas dataframe
+def compile_data():
+    global df
+    global dep_times_list
+    global arr_times_list
+    global airlines_list
+    global price_list
+    global durations_list
+    global stops_list
+    global layovers_list
+
+    dep_times = browser.find_elements_by_xpath("//span[@data-test-id='departure-time']")
+    dep_times_list = [value.text for value in dep_times]
+    
+    arr_times = browser.find_elements_by_xpath("//span[@data-test-id='arrival-time']")
+    arr_times_list = [value.text for value in arr_times]
+   
+    airlines = browser.find_elements_by_xpath("//span[@data-test-id='airline-name']")
+    airlines_list = [value.text for value in airlines]
+   
+    prices = browser.find_elements_by_xpath("//span[@data-test-id='listing-price-dollars']")
+    price_list = [value.text for value in prices]
+   
+    durations = browser.find_elements_by_xpath("//span[@data-test-id='duration']")
+    durations_list = [value.text for value in durations]
+    
+    stops = browser.find_elements_by_xpath("//span[@class='number-stops']")
+    stops_list = [value.text for value in stops]
+    
+    layovers = browser.find_elements_by_xpath("//span[@data-test-id='layover-airport-stops']")
+    layovers_list = [value.text for value in layovers]
+    
+    #getting the optimal values for the feilds 
+    now = datetime.datetime.now()
+    current_date = (str(now.year) + '-' + str(now.month) + '-' + str(now.day))
+    current_time = (str(now.hour) + ':' + str(now.minute))
+    current_price = 'price' + '(' + current_date + '---' + current_time + ')'
+    
+    for i in range(len(dep_times_list)):
+        try:
+            df.loc[i, 'departure_time'] = dep_times_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, 'arrival_time'] = arr_times_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, 'airline'] = airlines_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, 'duration'] = durations_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, 'stops'] = stops_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, 'layovers'] = layovers_list[i]
+        except Exception as e:
+            pass
+        try:
+            df.loc[i, str(current_price)] = price_list[i]
+        except Exception as e:
+            pass
+    print('Data Frame converted into Excel Sheet')
+
+#mail id by which the mail will go to you
+username = 'sweetshruti110096@gmail.com'
+password = 'XXXXXXXXXXXX'
+
+# Connecting over email
+def connect_mail(username, password):
+    global server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+
+
+# Creating message template for email
+def create_msg():
+    global msg
+    msg = '\nCurrent Cheapest flight:\n\nDeparture time: {}\nArrival time: {}\nAirline: {}\nFlight duration: {}\nNo. of stops: {}\nPrice: {}\n'.format(
+        cheapest_dep_time,
+        cheapest_arrival_time,
+        cheapest_airline,
+        cheapest_duration,
+        cheapest_stops,
+        cheapest_price)
+
+# Sending message email
+def send_email(msg):
+    global message
+    message = MIMEMultipart()
+    message['Subject'] = 'Current Best flight'
+    message['From'] = 'sweetshruti110096@gmail.com'
+    message['to'] = 'spj11@iitbbs.ac.in'
+    server.sendmail('sweetshruti110096@gmail.com', 'spj11@iitbbs.ac.in', msg)
+
+for i in range(4): # 4 times after every one hour
+    link = 'https://www.expedia.co.in/' #easy to access website comparitively
+    browser.get(link)
+
+    time.sleep(5)
+    # choose flights only
+    flights_only = browser.find_element_by_xpath("//button[@id='tab-flight-tab-hp']")
+    flights_only.click()
+     # choose flight type
+    ticket_chooser(return_ticket)
+     # choosing the source and destination
+    dep_country_chooser('Bangalore')
+    arrival_country_chooser('Bhubaneswar')
+     # choosing the dep date and arr date
+    dep_date_chooser('04', '07', '2019')
+    return_date_chooser('05', '07', '2019')
+    search()
+    compile_data()
+    # saving values for email
+    current_values = df.iloc[0]
+    cheapest_dep_time = current_values[0]
+    cheapest_arrival_time = current_values[1]
+    cheapest_airline = current_values[2]
+    cheapest_duration = current_values[3]
+    cheapest_stops = current_values[4]
+    cheapest_price = current_values[-1]
+    print('Run completed!')
+    connect_mail(username, password)
+    create_msg()
+    send_email(msg)
+
+    print('Email sent!')
+    df.to_excel('flights.xlsx')
+    time.sleep(3600)
